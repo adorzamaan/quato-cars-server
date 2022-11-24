4e39b8807f5163d0b8f942b9a4d0de756167c044
Ador Zamaan <adorzamaan@gmail.com>
2022-11-24 19:39:23 +0600

    user db store

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 require("colors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -28,6 +29,37 @@ async function dbConnect() {
 
 dbConnect().catch((err) => console.log(err.message));
 
+const userCollection = client.db("QuatoCarsDb").collection("users");
+
+app.get("/jwt", async (req, res) => {
+  const email = req.query.email;
+  const query = { email: email };
+  const user = await userCollection.findOne(query);
+  if (user) {
+    const token = jwt.sign({ email }, process.env.access_token, {
+      expiresIn: "365d",
+    });
+    return res.send({ accessToken: token });
+  }
+  return res.status(403).send({ accessToken: "Forbidden Access" });
+});
+
+app.post("/users", async (req, res) => {
+  try {
+    const user = req.body;
+    const result = await userCollection.insertOne(user);
+    res.send(result);
+  } catch (error) {
+    res.send(error.message);
+  }
+});
+app.get("/users", async (req, res) => {
+  try {
+    const query = {};
+    const result = await userCollection.find(query).toArray();
+    res.send(result);
+  } catch (error) {}
+});
 app.get("/", (req, res) => {
   res.send("I'm from backend");
 });
